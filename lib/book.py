de5854e194ea322301590f4406fa12f0046fdc57
Ryan <coap.ryanmontgomery@gmail.com>
2024-06-26 23:32:43 -0600

    put all together

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,33 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+        def __init__(self, title = "And Then There Were None", page_count = 272):
+            self.title = title
+            self.page_count = page_count
+          
+        @property
+        def page_count(self):
+            """The page_count property"""
+            return self._page_count
+         
+        @page_count.setter
+        def page_count(self, page_count):
+                """page_count must be an integer" if page_count is not an integer."""
+                if isinstance(page_count, int):
+                    self._page_count = page_count
+                else:
+                    raise ValueError("page_count must be an integer.")
+
+            
+        def turn_page(self):
+            print("Flipping the page...wow, you read fast!")
+            
+            
+            
+        # end def
+        # end def
+        # end alternate constructor
+    
+
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    
+    def __init__(self, brand, size):
+        if isinstance(size, int):
+            self.brand = brand
+            self.size = size
+        else:
+            raise ValueError("size must be an integer.")
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, size):
+        if isinstance(size, int):
+            self._size = size
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
